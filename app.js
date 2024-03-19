85bfd813eb6fcebda0bfb4b660d53d51a87e466a
repenaattori <reimarii@oamk.fi>
2024-03-19 19:08:48 +0200

    Register and login

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const student = require('./routes/student');
+const auth = require('./routes/authentication');
 
 const express = require('express');
 const app = express();
@@ -10,7 +11,9 @@ app.use( express.urlencoded({extended: false}) );
 app.use(upload.none());
 app.use(express.json());
 
+//Adding routes
 app.use('/student', student);
+app.use('/auth',  auth);
 
 const PORT = process.env.PORT || 3001;
 
